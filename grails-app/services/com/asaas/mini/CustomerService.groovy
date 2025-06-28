1ef85add0f873ddc98d4a05bd18d9f977764b3f8
bruna-giovanella <brugiovanella@gmail.com>
2025-06-28 16:34:43 -0300

    Criado save e validateCustomerParams para Customer em Service

--- grails-app/services/com/asaas/mini/CustomerService.groovy
+++ grails-app/services/com/asaas/mini/CustomerService.groovy
@@ -1,11 +1,74 @@
 package com.asaas.mini
 
 import javax.transaction.Transactional
+import javax.xml.bind.ValidationException
 
 @Transactional // faz com que o método aconteça por completo, ou de rollback
 class CustomerService {
 
-    //cadastro de customer
+    //Cadastro de customer
+    public Customer save(Map params) {
+        Customer customerValues = validateCustomerParams(params)
+
+        if (customerValues.hasErrors()) {
+            throw new ValidationException("Error creating customer", customerValues.errors)
+        }
+        if (Customer.findByCpfCnjp(params.cpfCnpj)) {
+            // verificar duplicidade de documento
+            customerValues.errors.rejectValue("cpfcnpj", "cpfCnpj.exists", "There is already a customer with this CPF/CNPJ")
+            throw new ValidationException("Error creating customer", customerValues.errors)
+        }
+        if (Customer.findByEmail(params.email)) {
+            // verificar duplicidade de email
+            customerValues.errors.rejectValue("email", "email.exists", "Já existe um customer com esse email")
+            throw new ValidationException("Error creating customer", customerValues.errors)
+        }
+
+        Address address = new Address()
+        address.cep = params.cep
+        address.city = params.city
+        address.state = params.state
+        address.complement = params.complement
+
+        Customer customer = new Customer()
+        customer.name = params.name
+        customer.email = params.email
+        customer.cpfCnpj = params.cpfCnpj
+        customer.address = address
+
+        return customer.save(flush: true, failOnError: true)
+
+    }
+
+    private Customer validateCustomerParams(Map params) {
+        Customer customer = new Customer();
+
+        if (!params.name?.trim()) {
+            customer.errors.rejectValue("name", "name.blank", "Name cannot be empty")
+        } else if (params.name.length() > 255) {
+            customer.errors.rejectValue("name", "name.maxSize", "Name must have a maximum of 255 characters")
+        }
+
+        if (!params.email?.trim()) {
+            customer.errors.rejectValue("email", "email.blank", "Email cannot be empty")
+        } else if (params.email.length() > 255) {
+            customer.errors.rejectValue("email", "email.maxSize", "Email must have a maximum of 255 characters")
+        } else if (!(params.email ==~ /^[^@\s]+@[^@\s]+\.[^@\s]+$/)) {
+            customer.errors.rejectValue("email", "email.invalid", "Email invalid")
+        }
+
+        if (!params.cpfCnpj?.trim()) {
+            customer.errors.rejectValue("cpfCnpj", "cpfCnpj.blank", "CPF/CNPJ cannot be empty")
+        } else if (!(params.cpfCnpj ==~ /\d{11}|\d{14}/)) {
+            customer.errors.rejectValue("cpfCnpj", "cpfCnpj.invalidFormat", "CPF/CNPJ invalid")
+        }
+
+        if (!params.address) {
+            customer.errors.rejectValue("address", "address.required", "Address is required")
+        }
+
+        return customer
+    }
 
     //Atualização de customer
 
@@ -15,7 +78,7 @@ class CustomerService {
 
     //Soft delete
     public void deleteCustomer(Long id) {
-        Customer customer = customer.findByIdAndDeleted(id, false); // procura no banco pelo ID e nao deletado
+        Customer customer = Customer.findByIdAndDeleted(id, false); // procura no banco pelo ID e nao deletado
 
         if (!customer) {
             throw new IllegalArgumentException("Customer not found")
@@ -33,7 +96,7 @@ class CustomerService {
 
     //restore
     public void restoreCustomer(Long id) {
-        Customer customer = customer.findByIdAndDeleted(id, true)
+        Customer customer = Customer.findByIdAndDeleted(id, true)
 
         if (!customer) {
             throw new IllegalArgumentException("Customer not found or is not deleted")
