ce4d37fe4db0afd66e4cf8aecede5395eba6abe0
bruna-giovanella <brugiovanella@gmail.com>
2025-07-05 13:37:16 -0300

    conectado ao mysql, banco 'mini-asaas'

--- grails-app/controllers/com/asaas/mini/CustomerController.groovy
+++ grails-app/controllers/com/asaas/mini/CustomerController.groovy
@@ -3,6 +3,7 @@ package com.asaas.mini
 import org.grails.datastore.mapping.validation.ValidationException
 
 class CustomerController {
+
     static responseFormats = ['json']
     CustomerService customerService // injeção de dependência automática
 
@@ -10,6 +11,14 @@ class CustomerController {
         try {
             def customer = customerService.save(params)
             respond customer, [status: 201] // para retornar Json 201 (created)
+        } catch (ValidationException e) {
+            render(status: 400, contentType: 'application/json', text: [errors: e.errors.allErrors*.defaultMessage].toString())
+        }
+    }
+
+    def delete() {
+        try {
+            def customer = customerService.deleteCustomer(params.id)
         } catch (ValidationException e) {
             render(status: 400, contentType: 'application/json', text: [errors: e.errors.allErrors*.defaultMessage].toString())
         }
