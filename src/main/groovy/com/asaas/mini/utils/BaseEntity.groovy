1296589473d4c2903dae082d6e1ad64fd08ac664
bruna-giovanella <brugiovanella@gmail.com>
2025-07-07 09:41:19 -0300

    Adicionado quebra de linhas e nullable em lastUpdate

--- src/main/groovy/com/asaas/mini/utils/BaseEntity.groovy
+++ src/main/groovy/com/asaas/mini/utils/BaseEntity.groovy
@@ -2,8 +2,14 @@ package com.asaas.mini.utils
 
 class BaseEntity {
     Date dateCreated
+
     Date lastUpdated
+
     Boolean deleted = false
+
+    static constraints = {
+        lastUpdate nullable: true
+    }
 
     static mapping = {
         deleted column: 'deleted', sqlType: 'tinyint(1)', defaultValue: 0
