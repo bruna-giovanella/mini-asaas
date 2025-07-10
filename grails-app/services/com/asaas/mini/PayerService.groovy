aba00f25ac1a5e955175892c26d5c036ca7b5451
bruna-giovanella <brugiovanella@gmail.com>
2025-07-10 11:13:27 -0300

    Ajustado validação unica para save e encapsulado toda validação dentro de validateSave

--- grails-app/services/com/asaas/mini/PayerService.groovy
+++ grails-app/services/com/asaas/mini/PayerService.groovy
@@ -7,20 +7,10 @@ import org.grails.datastore.mapping.validation.ValidationException
 class PayerService {
 
     public Payer save(Map params, Customer customer) {
-        Payer payerValues = validatePayerParams(params)
+        Payer payerValidate = validateSave(params)
 
-        if (payerValues.hasErrors()) {
-            throw new ValidationException("Error creating payer", payerValues.errors)
-        }
-
-        if (Payer.findByCpfCnpjAndCustomer(params.cpfCnpj, customer)) {
-            payerValues.errors.rejectValue("cpfCnpj", "cpfCnpj.exists", "There is already a payer with this CPF/CNPJ for this customer")
-            throw new ValidationException("Error creating payer", payerValues.errors)
-        }
-
-        if (Payer.findByEmailAndCustomer(params.email, customer)) {
-            payerValues.errors.rejectValue("email", "email.exists", "There is already a payer with this email for this customer")
-            throw new ValidationException("Error creating payer", payerValues.errors)
+        if (payerValidate.hasErrors()) {
+            throw new ValidationException("Error creating payer", payerValidate.errors)
         }
 
         Address address = new Address()
@@ -40,14 +30,23 @@ class PayerService {
         return payer.save(flush: true, failOnError: true)
     }
 
-    private Payer validatePayerParams(Map params) {
+    private Payer validateSave(Map params) {
         Payer payer = new Payer()
+
+        if (Payer.findByCpfCnpjAndCustomer(params.cpfCnpj, customer)) {
+            payer.errors.rejectValue("cpfCnpj", "cpfCnpj.exists", "There is already a payer with this CPF/CNPJ for this customer")
+        }
+
+        if (Payer.findByEmailAndCustomer(params.email, customer)) {
+            payer.errors.rejectValue("email", "email.exists", "There is already a payer with this email for this customer")
+        }
 
         if (!params.name?.trim()) {
             payer.errors.rejectValue("name", "name.blank", "Name cannot be empty")
         } else if (params.name.length() > 255) {
             payer.errors.rejectValue("name", "name.maxSize", "Name must have a maximum of 255 characters")
         }
+
         if (!params.email?.trim()) {
             payer.errors.rejectValue("email", "email.blank", "Email cannot be empty")
         } else if (params.email.length() > 255) {
@@ -55,26 +54,30 @@ class PayerService {
         } else if (!(params.email ==~ /^[^@\s]+@[^@\s]+\.[^@\s]+$/)) {
             payer.errors.rejectValue("email", "email.invalid", "Invalid email")
         }
+
         if (!params.contactNumber?.trim()) {
             payer.errors.rejectValue("contactNumber", "contactNumber.blank", "Contact number cannot be empty")
         } else if (!(params.contactNumber ==~ /^(1[1-9]|2[12478]|3[1234578]|4[12345789]|5[1345]|6[1]|7[134579]|8[1-9]|9[1-9])\d{8,9}$/)) {
             payer.errors.rejectValue("contactNumber", "contactNumber.invalidFormat", "Invalid contact number")
         }
+
         if (!params.cpfCnpj?.trim()) {
             payer.errors.rejectValue("cpfCnpj", "cpfCnpj.blank", "CPF/CNPJ cannot be empty")
         } else if (!(params.cpfCnpj ==~ /\d{11}|\d{14}/)) {
             payer.errors.rejectValue("cpfCnpj", "cpfCnpj.invalidFormat", "Invalid CPF/CNPJ")
         }
+
         if (!params.cep?.trim()) {
             payer.errors.rejectValue("address", "address.cep.blank", "CEP cannot be empty")
         }
+
         if (!params.city?.trim()) {
             payer.errors.rejectValue("address", "address.city.blank", "City cannot be empty")
         }
+
         if (!params.state?.trim()) {
             payer.errors.rejectValue("address", "address.state.blank", "State cannot be empty")
         }
-
         return payer
     }
 
