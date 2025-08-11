107dfe41c3c29e9368f1e863124c4fb9a56a56c4
Bru <166079160+bruna-giovanella@users.noreply.github.com>
2025-08-11 11:20:41 -0300

    fix: retirado extend BaseEntity em Role

--- grails-app/domain/com/asaas/mini/auth/Role.groovy
+++ grails-app/domain/com/asaas/mini/auth/Role.groovy
@@ -2,7 +2,7 @@ package com.asaas.mini.auth
 
 import com.asaas.mini.utils.BaseEntity
 
-class Role extends BaseEntity implements Serializable {
+class Role implements Serializable {
     String authority
 
     static mapping = {
